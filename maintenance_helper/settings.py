52c099fa3c1026408f21a885ef09131787db6c86
eftoG <evtim.m.georgiev@gmail.com>
2022-12-21 20:53:21 +0100

    Configuration improvements

--- maintenance_helper/settings.py
+++ maintenance_helper/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from django.urls import reverse_lazy
@@ -9,7 +10,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j(o8$aznf(fu)q+l-5-$sn=pr4sl5&xe(959lvnu^-3c(f3ox!'
+SECRET_KEY = config("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
